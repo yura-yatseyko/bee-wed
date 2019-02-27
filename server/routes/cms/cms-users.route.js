1340abfd6866f2bdebef3f3b450bd53ab27434ca
Yatseyko Yuriy <yura.volpis@gmail.com>
2019-02-27 20:01:24 +0200

    Added exel.

--- server/routes/cms/cms-users.route.js
+++ server/routes/cms/cms-users.route.js
@@ -81,44 +81,52 @@ router.get('/cms/users/bridegroom', authenticate, async (req, res) => {
 });
 
 router.get('/cms/users/toexel', authenticate, (req, res) => {
-    var query = {
-        kind: "BrideGroomUser",
-        isSubscribedToNewsletter: true,
-    }
-
-    User.find(query).then((users) => {
-        var modifiedUsers = [];
-        users.forEach(function(user) {
-            let newUser = {
-                _id: user._id,
-                email: user.email,
-                name: user.name,
-                registerDate: user._id.getTimestamp().getTime(),
-                supplierType: user.supplierType,
-                phone: user.phone,
-                websiteURL: user.websiteURL,
-            }
-
-            modifiedUsers.push(newUser);
-        });
-
-        var model = mongoXlsx.buildDynamicModel(modifiedUsers);
-
-        /* Generate Excel */
-        mongoXlsx.mongoData2Xlsx(modifiedUsers, model, function(err, data) {
-            console.log(err);
-            console.log(data);
-            
-            res.status(200).send({
-                success: true,
-                data: {
-                    url: data.fullPath
+    var conf ={};
+    conf.stylesXmlFile = "styles.xml";
+    conf.name = "mysheet";
+    conf.cols = [{
+        caption:'string',
+        type:'string',
+        beforeCellWrite:function(row, cellData){
+             return cellData.toUpperCase();
+        },
+        width:28.7109375
+    },{
+        caption:'date',
+        type:'date',
+        beforeCellWrite:function(){
+            var originDate = new Date(Date.UTC(1899,11,30));
+            return function(row, cellData, eOpt){
+                if (eOpt.rowNum%2){
+                    eOpt.styleIndex = 1;
+                }  
+                else{
+                    eOpt.styleIndex = 2;
                 }
-            });
-        });
-    }).catch((e) => {
-        res.status(400).send();
-    });
+                if (cellData === null){
+                  eOpt.cellType = 'string';
+                  return 'N/A';
+                } else
+                  return (cellData - originDate) / (24 * 60 * 60 * 1000);
+            } 
+        }()
+    },{
+        caption:'bool',
+        type:'bool'
+    },{
+        caption:'number',
+        type:'number'               
+    }];
+    conf.rows = [
+        ['pi', new Date(Date.UTC(2013, 4, 1)), true, 3.14],
+        ["e", new Date(2012, 4, 1), false, 2.7182],
+        ["M&M<>'", new Date(Date.UTC(2013, 6, 9)), false, 1.61803],
+        ["null date", null, true, 1.414]  
+    ];
+    var result = nodeExcel.execute(conf);
+    res.setHeader('Content-Type', 'application/vnd.openxmlformats');
+    res.setHeader("Content-Disposition", "attachment; filename=" + "Report.xlsx");
+    res.end(result, 'binary');
 });
 
 router.get('/cms/users/subscribed', authenticate, async (req, res) => {
