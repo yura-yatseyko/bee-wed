51c9db96681376e99027788c0446bcd56bd9f420
Yatseyko Yuriy <yura.volpis@gmail.com>
2019-08-14 18:55:47 +0300

    Small fixes.

--- server/routes/user.route.js
+++ server/routes/user.route.js
@@ -65,8 +65,12 @@ router.get('/image/:id', function (req, res, next) {
         return
       }
   
-      res.setHeader('Content-Length', resp.headers['content-length'])
-      res.setHeader('Content-Type', resp.headers['content-type'])
+    //   res.setHeader('Content-Length', resp.headers['content-length'])
+    //   res.setHeader('Content-Type', resp.headers['content-type'])
+    res.setHeader('Content-Length', '7974790')
+      res.setHeader('Content-Type', 'binary/octet-stream')
+      res.setHeader('Connection', 'keep-alive')
+      res.setHeader('Accept-Ranges', 'bytes')
   
       // cache-control?
       // etag?
