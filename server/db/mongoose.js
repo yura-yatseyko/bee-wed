3bd0a4533a4428817605cc742de0d3ae495c8e1c
Yatseyko Yuriy <yura.volpis@gmail.com>
2019-08-27 17:54:28 +0300

    Small fixes.

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -3,7 +3,6 @@ var mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
 
 mongoose.set('useCreateIndex', true)
-mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true });
 
 run().then(() => console.log('done')).catch(error => console.error(error.stack));
 
