f76b64ef07155f75930a89b1344cdc4ba358ed6e
Yatseyko Yuriy <yura.volpis@gmail.com>
2019-09-04 16:36:46 +0300

    Small fixes.

--- server/routes/chat.route.js
+++ server/routes/chat.route.js
@@ -168,7 +168,7 @@ router.get('/chat/messages/:receiverId', authenticate, async (req, res) => {
 router.get('/chat-edited', authenticate, async (req, res) => {   
     const pipeLine = [{
         $project: {
-            trsenderipId: 1,
+            sender: 1,
             receiver: 1,
         }
     }, {
