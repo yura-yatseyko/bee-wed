5d370f5288dbef46adb97347f108a516dfa59277
Yatseyko Yuriy <yura.volpis@gmail.com>
2019-09-04 14:59:58 +0300

    Small fixes.

--- server/routes/chat.route.js
+++ server/routes/chat.route.js
@@ -191,12 +191,10 @@ router.get('/chat', authenticate, async (req, res) => {
                     if (req.user._id.equals(msg.sender._id)) {
                         if (element.chatWithUser._id.equals(msg.receiver._id)) {
                             found = true;
-                            break;
                         }
                     } else {
                         if (element.chatWithUser._id.equals(msg.sender._id)) {
                             found = true;
-                            break;
                         }
                     }
                 });  
